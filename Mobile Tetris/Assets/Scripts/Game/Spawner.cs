518d3f0fbea02185027dd2c1dca908c881dcd7ca
Matija Huremović <matija.huremovic@gmail.com>
2021-06-03 01:16:18 +0200

    Spawned game pieces will now be of random colour

--- Mobile Tetris/Assets/Scripts/Game/Spawner.cs
+++ Mobile Tetris/Assets/Scripts/Game/Spawner.cs
@@ -5,10 +5,18 @@ public class Spawner : MonoBehaviour
     [SerializeField]
     private GameObject[] spawnablePieces;
 
+    private Color[] colors = new Color[] { Color.red, Color.blue, Color.green, Color.yellow, Color.cyan, Color.magenta };
+
     public GameObject SpawnPiece()
     {
-        int randomIndex = Random.Range(0, spawnablePieces.Length);
+        GameObject temp = Instantiate(spawnablePieces[Random.Range(0, spawnablePieces.Length)], transform.position, Quaternion.identity);
 
-        return Instantiate(spawnablePieces[randomIndex], transform.position, Quaternion.identity);
+        Color randomColor = colors[Random.Range(0, colors.Length)];
+        foreach(SpriteRenderer spriteRenderer in temp.GetComponentsInChildren<SpriteRenderer>())
+        {
+            spriteRenderer.color = randomColor;
+        }
+
+        return temp;
     }
 }
